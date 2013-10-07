838911140fd7c0a86072868401c67fb760cf9de2
Cheepsz <cheepsz@gmail.com>
2013-10-07 16:29:11 -0500

    myitem class acceleration example

---
+++ movingObj/myitem2.cpp
@@ -0,0 +1,81 @@
+#include "myitem.h"
+#include <iostream>
+using namespace std;
+
+myItem::myItem()
+{
+    speed = 0.1;
+
+    //angle = (qrand()%360);
+    //setRotation(angle);
+
+    int startx = 0;
+    int starty = 0;
+
+    if((qrand()%1)){
+        startx = (qrand()%200);
+        starty = (qrand()%200);
+    }
+    else{
+        startx = (qrand()%-100);
+        starty = (qrand()%-100);
+    }
+    setPos(mapToParent(startx,starty));
+}
+
+QRectF myItem::boundingRect() const
+{
+    return QRect(0,0,20,20);
+}
+
+void myItem::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+    QRectF rec = boundingRect();
+
+    QBrush brush(Qt::gray);
+
+    if(scene()->collidingItems(this).isEmpty()){
+        brush.setColor(Qt::green);
+    }
+    else{
+        brush.setColor(Qt::red);
+
+        DoCollison();//set new position
+    }
+
+    painter->fillRect(rec,brush);
+    painter->drawRect(rec);
+}
+
+void myItem::advance(int phase)
+{
+    if(!phase) return;
+    speed += 0.001;
+    QPointF location = this->pos();
+    setPos(mapToParent(0,-(speed)));//maps current x,y coordinates to the scene x,y coordinates
+}
+
+void myItem::increaseSpeed()
+{
+    speed += 0.1;
+    cout << "hello";
+}
+
+void myItem::DoCollison()
+{
+    if((qrand()%1)){
+        setRotation(rotation()+(180 + (qrand()%10)));
+    }
+    else{
+        setRotation(rotation()+(180 + (qrand()%-10)));
+    }
+
+    QPointF newPoint = mapToParent(-(boundingRect().width()),-(boundingRect().width()+2));
+
+    if(!scene()->sceneRect().contains(newPoint)){
+        newPoint = mapFromParent(0,0);
+    }
+    else{
+        setPos(newPoint);
+    }
+}
